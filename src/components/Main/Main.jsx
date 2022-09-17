f32cc59aea3b89efec18cbad248878fafb662f75
Ihor <ihorfront88@gmail.com>
2022-09-17 19:32:08 +0300

    add task-4

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import Profile from 'components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
 import user from '../../data/user';
 import data from '../../data/data';
 import friends from '../../data/friends';
+import transactions from '../../data/transactions';
 
 const Main = () => {
   return (
@@ -17,6 +19,7 @@ const Main = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </main>
   );
 };
