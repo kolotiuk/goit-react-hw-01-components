6fcd76c47bb8d2709a2536823b5e570653038e0b
Ihor <ihorfront88@gmail.com>
2022-09-18 10:56:17 +0300

    add absolute path to all files

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,10 +2,10 @@ import Profile from 'components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
 import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
-import user from '../../data/user';
-import data from '../../data/data';
-import friends from '../../data/friends';
-import transactions from '../../data/transactions';
+import user from 'data/user';
+import data from 'data/data';
+import friends from 'data/friends';
+import transactions from 'data/transactions.json';
 
 const Main = () => {
   return (
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import s from './Statistics.module.scss';
-import { getRandomHexColor } from './getRandomHexColor';
+import getRandomHexColor from './getRandomHexColor';
 
 const Statistics = props => {
   const { title, stats } = props;
